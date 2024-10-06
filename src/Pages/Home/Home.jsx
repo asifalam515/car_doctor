abe3d040ae8e9c743f1eea41ac0eeaa070505734
asifalam515 <asibulalam515@gmail.com>
2024-10-06 09:10:19 +0600

    about page started

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,11 @@
+import About from "./About/About";
+import Banner from "./Banner/Banner";
+
 const Home = () => {
   return (
     <div>
-      <h1 className="text-5xl">This is Home</h1>
+      <Banner></Banner>
+      <About></About>
     </div>
   );
 };
